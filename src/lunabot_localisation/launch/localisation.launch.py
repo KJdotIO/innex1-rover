d7e2f9864b167598985f183323a1583adc927e18
jm989 <martinsjayden79@gmail.com>
2026-02-07 16:23:05 +0000

    Add missing blank line for PEP8 compliance (E302)

--- src/lunabot_localisation/launch/localisation.launch.py
+++ src/lunabot_localisation/launch/localisation.launch.py
@@ -3,6 +3,7 @@ from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
     ekf_config_path = PathJoinSubstitution([
         get_package_share_directory('lunabot_localisation'),
