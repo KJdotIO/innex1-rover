e1e2e0525059c6c9bdd4b27f63d906db386f7ed0
KJdotIO <98490529+KJdotIO@users.noreply.github.com>
2026-04-12 13:34:34 +0100

    Clean navigation and perception package scaffolding (#222)

--- src/lunabot_perception/test/test_copyright.py
+++ src/lunabot_perception/test/test_copyright.py
@@ -14,15 +14,17 @@
 
 """Test copyright headers in source files."""
 
-from ament_copyright.main import main
 import pytest
+from ament_copyright.main import main
 
 
-# Remove the `skip` decorator once the source file(s) have a copyright header
-@pytest.mark.skip(reason='No copyright header has been placed in the generated source file.')
+# Remove the `skip` decorator once source files have copyright headers.
+@pytest.mark.skip(
+    reason="No copyright header has been placed in the generated source file."
+)
 @pytest.mark.copyright
 @pytest.mark.linter
 def test_copyright():
     """Test that all source files have proper copyright headers."""
-    rc = main(argv=['.', 'test'])
-    assert rc == 0, 'Found errors'
+    rc = main(argv=[".", "test"])
+    assert rc == 0, "Found errors"
--- src/lunabot_perception/test/test_pep257.py
+++ src/lunabot_perception/test/test_pep257.py
@@ -14,13 +14,13 @@
 
 """Test docstrings follow PEP257 conventions."""
 
-from ament_pep257.main import main
 import pytest
+from ament_pep257.main import main
 
 
 @pytest.mark.linter
 @pytest.mark.pep257
 def test_pep257():
     """Test that code follows PEP257 docstring conventions."""
-    rc = main(argv=['.', 'test'])
-    assert rc == 0, 'Found code style errors / warnings'
+    rc = main(argv=[".", "test"])
+    assert rc == 0, "Found code style errors / warnings"
